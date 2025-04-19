1f123e3a2c319e30f078fa198963e9283df710a9
FelipePaparelo <felipe_paparelo01@hotmail.com>
2025-04-19 18:11:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import FirstComponent from "./components/FirstComponent";
+import LoginLayout from "./layouts/loginLayout";
+
+ 
+function App() {
+  return (
+    // <FirstComponent></FirstComponent>
+    <LoginLayout></LoginLayout>
+  )
+}
+
+export default App;
---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,8 @@
+function FirstComponent(){
+    const mensaje = "react developer";
+    if(mensaje){
+        return <p>Hola {mensaje}</p>
+    }
+    return <p>Hola masito</p>
+}
+export default FirstComponent;
